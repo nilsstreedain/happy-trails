1c2936edd5ff93bad4a02a5c790dc0f5ef94eb8f
Nils Streedain <nils@streedain.com>
2022-11-17 15:43:46 -0800

    Implemented hike button functionality

--- Hiking-Trails/Hiking-Trails/Views/LiveHikeView.swift
+++ Hiking-Trails/Hiking-Trails/Views/LiveHikeView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 import MapKit
 
 struct LiveHikeView: View {
+	
+	@ObservedObject var currHike = Current_Hike()
+	
     var body: some View {
 		VStack {
 			MapUIView()
 				.ignoresSafeArea()
 			HStack() {
 				Spacer()
-				Label("0:00:00", systemImage: "timer")
+				Label(String(format: "%02d", currHike.counter / 60) + ":" + String(format: "%02d", currHike.counter % 60), systemImage: "timer")
 				Spacer()
 				Label("0 MI", systemImage: "lines.measurement.horizontal")
 				Spacer()
@@ -29,14 +32,17 @@ struct LiveHikeView: View {
 				Label("0 FT", systemImage: "mountain.2")
 				Spacer()
 			}.padding(5)
-			Button {
-//				saveUser()
-			} label: {
-				Text("Start Hike").frame(maxWidth: .infinity)
-					.padding(5)
+			HStack() {
+				if currHike.mode == .stopped {
+					hikeButton(label: "Start Hike", color: Color("AccentColor"), op: self.currHike.start)
+				} else if currHike.mode == .started {
+					hikeButton(label: "Pause Hike", color: Color.orange, op: self.currHike.pause)
+					hikeButton(label: "End Hike", color: Color.red, op: self.currHike.reset)
+				} else if currHike.mode == .paused {
+					hikeButton(label: "Resume Hike", color: Color("AccentColor"), op: self.currHike.start)
+					hikeButton(label: "End Hike", color: Color.red, op: self.currHike.reset)
+				}
 			}
-			.buttonStyle(.borderedProminent)
-			.padding(10)
 		}
     }
 }
@@ -45,4 +51,51 @@ struct LiveHikeView_Previews: PreviewProvider {
     static var previews: some View {
         LiveHikeView()
     }
+}
+
+struct hikeButton: View {
+	let label: String
+	let color: Color
+	let op: () -> ()
+	
+	var body: some View {
+		Button(action: {op()}) {
+  			Text(label)
+			.frame(maxWidth: .infinity)
+  			.padding(5)
+  		}
+  		.buttonStyle(.borderedProminent)
+  		.accentColor(color)
+  		.padding(10)
+	}
+}
+
+class Current_Hike: ObservableObject {
+	@Published var counter: Int = 0
+	@Published var mode: hikeMode = .stopped
+	var timer = Timer()
+	
+	enum hikeMode {
+		case started
+		case paused
+		case stopped
+	}
+	
+	func start() {
+		mode = .started
+		self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+			self.counter += 1
+		}
+	}
+	
+	func pause() {
+		mode = .paused
+		self.timer.invalidate()
+	}
+	
+	func reset() {
+		mode = .stopped
+		self.counter = 0
+		self.timer.invalidate()
+	}
 }
